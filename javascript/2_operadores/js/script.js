9cc640d379eb6d019bda032ab854595ffe8f4364
Iago <iago.g.freitas@gmail.com>
2024-08-26 23:47:59 -0300

    nova aula

---
+++ javascript/2_operadores/js/script.js
@@ -0,0 +1,25 @@
+// 1 - Number
+console.log(typeof 2);
+console.log(typeof 5.14);
+console.log(typeof -127);
+
+// Operaçoes aritmética
+console.log(2 + 4);
+console.log(10 - 5);
+console.log(5 * 4);
+console.log(10 / 2);
+console.log(5 + 4 * 12);
+console.log(5 + (4 * 12 - 6 / 2));
+console.log(5 + (4 * 12) / 2);
+
+// 3 - Special numbers
+console.log(typeof Infinity);
+console.log(typeof -Infinity);
+
+console.log(12 * "asd");
+console.log(typeof NaN);
+
+
+// 4 - Strings
+console.log("Um texto");
+console.log(`Mais um texto`);
